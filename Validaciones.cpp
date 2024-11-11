5250ffb28865ebcd11d24b8a6154c940e14f9a2e
Josue-Chango <josuechango21@hotmail.com>
2024-11-10 21:50:39 -0500

    Ejercicio completo

--- Validaciones.cpp
+++ Validaciones.cpp
@@ -1,202 +1,153 @@
 /***********************************************************************
  *            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE                  *
- * Proposito:                      Programar una suma fracciones       *
+ * Proposito:                      Programa con validacion de entrada  *
  * Autor:                          Josue Chango                        *
- * Fecha de creacion:              06/10/2024                          *
- * Fecha de modificacion:          07/11/2024                          *
+ * Fecha de creacion:              09/11/2024                          *
+ * Fecha de modificacion:          10/11/2024                          *
  * Materia:                        Estructura de datos                 *
  * NRC :                           1978                                *
  ***********************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
+#include "Validaciones.h"
 #include <iostream>
+#include <string>
+#include <conio.h>
+#include <cstdlib>
 
 using namespace std;
 
+template <typename T>
+Validaciones<T>::Validaciones()
+{
+}
 
-//Ingresar enteros------------------------------------------------------------------------
-int ingresar(char *);
-int ingresar(char *msj){
+template <typename T>
+T Validaciones<T>::ingresar(char *msj, char *tipo)
+{
     char cad[10];
     char c;
     int i = 0;
-    int valor = 0;
+    T valor;
     printf("%s", msj);
-    while((c = getch()) != 13){
-        if (c >= '0' && c <= '9'){
-            printf("%c", c);
-            cad[i++] = c;
-        } else if (c == 8) {
-            if (i > 0) {
-                printf("\b \b");
-                i--;
-            }
-        }
-    }
-    cad[i++] = '\0';
-    valor = atoi(cad);
-    return valor;
-}
-
-
-//Ingresar una letra-------------------------------------------------------------------------
-char ingresarLetra(char *);
-char ingresarLetra(char *msj) {
-    char c;
-    printf("%s", msj);
-    while (true) {
-        c = getch();
-        if (isalpha(c)) {
-            printf("%c", c);
-            return c;
-        } else if (c == 8) {
-            printf("\b \b");
-        }
-    }
-}
-
-
-
-//Ingresar float------------------------------------------------------------------------------
-float ingresarFloat(char *);
-float ingresarFloat(char *msj){
-    char cad[10];
-    char c;
-    int i = 0;
-    float valor = 0;
-    printf("%s", msj);
-    while((c = getch()) != 13){
-        if (c >= '0' && c <= '9'){
-            printf("%c", c);
-            cad[i++] = c;
-        } else if (c == 8) {
-            if (i > 0) {
-                printf("\b \b");
-                i--;
-            }
-        } else if (c == 46) {
-            bool aux = false;
-            for (int j = 0; cad[j] != '\0'; j++) {
-                if (cad[j] == 46){
-                    aux = true;
-                    break;
-                }
-            }
-            if(aux == false){
+    while ((c = getch()) != 13)
+    {
+        if (tipo == "entero")
+        {
+            if (c >= '0' && c <= '9')
+            {
                 printf("%c", c);
                 cad[i++] = c;
             }
+            else if (c == 8)
+            {
+                if (i > 0)
+                {
+                    printf("\b \b");
+                    i--;
+                }
+            }
         }
-    }
-    cad[i++] = '\0';
-    for (int j = 0; cad[j] != '\0'; j++) {
-        valor = atof(cad);
-    }
-    return valor;
-}
-
-//ingresar un string---------------------------------------------------------------------------------------
-string ingresarString(char *);
-string ingresarString(char *msj) {
-    char cad[10];
-    char c;
-    int i = 0;
-    string valor;
-    printf("%s", msj);
-    while ((c = getch()) != 13) {
-        if (isalpha(c)) {
-            printf("%c", c);
-            cad[i++] = c;
-        } else if (c == 8) {
-            if (i > 0) {
-                printf("\b \b");
-                i--;
+        else if (tipo == "flotante" || tipo == "double")
+        {
+            if (c >= '0' && c <= '9')
+            {
+                printf("%c", c);
+                cad[i++] = c;
+            }
+            else if (c == 8)
+            {
+                if (i > 0)
+                {
+                    printf("\b \b");
+                    i--;
+                }
+            }
+            else if (c == 46)
+            {
+                bool aux = false;
+                for (int j = 0; cad[j] != '\0'; j++)
+                {
+                    if (cad[j] == 46)
+                    {
+                        aux = true;
+                        break;
+                    }
+                }
+                if (aux == false)
+                {
+                    printf("%c", c);
+                    cad[i++] = c;
+                }
+            }
+        }
+        else if (tipo == "string")
+        {
+            if (isalpha(c))
+            {
+                printf("%c", c);
+                cad[i++] = c;
+            }
+            else if (c == 8)
+            {
+                if (i > 0)
+                {
+                    printf("\b \b");
+                    i--;
+                }
+            }
+        }
+        else if (tipo == "char")
+        {
+            if (isalpha(c))
+            {
+                printf("%c", c);
+                valor = c;
+                break;
+            }
+            else if (c == 8)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    printf("\b \b");
+                }
             }
         }
     }
     cad[i] = '\0';
-    for (int j = 0; cad[j] != '\0'; j++) {
-        valor = valor + cad[j];
+    if (tipo == "entero")
+    {
+        int entero;
+        entero = atoi(cad);
+        return valor = entero;
     }
-
-    return valor;
-}
-
-//Ingresar double------------------------------------------------------------------------------
-double ingresarDouble(char *msj){
-    char cad[20];
-    char c;
-    int i = 0;
-    double valor = 0;
-    printf("%s", msj);
-    bool puntoDecimal = false;
-    while((c = getch()) != 13){
-        if (c >= '0' && c <= '9'){
-            printf("%c", c);
-            cad[i++] = c;
-        } else if (c == 8) {
-            if (i > 0) {
-                printf("\b \b");
-                i--;
-            }
-        } else if (c == '.' && !puntoDecimal) {
-            puntoDecimal = true;
-            printf("%c", c);
-            cad[i++] = c;
+    else if (tipo == "flotante")
+    {
+        float flotante;
+        for (int j = 0; cad[j] != '\0'; j++)
+        {
+            flotante = atof(cad);
         }
+        return valor = flotante;
     }
-    cad[i] = '\0';
-    valor = atof(cad);
-    return valor;
-}
-
-int SumarEnteros(int obj1, int obj2){
-    int temporal;
-    temporal = obj1 + obj2;
-    return temporal;
-}
-
-float SumarFlotantes(float obj1, float obj2){
-    ingresarString("Ingrese su nombre: ");
-    float temporal;
-    temporal = obj1 + obj2;
-    return temporal;
-}
-
-
-
-
-int main(){
-    int numero1 = ingresar("ingrese un valor entero: ");
-    cout << endl;
-    int numero2 = ingresar("Ingrese un valor entero: ");
-    cout << endl;
-    int Resultado = SumarEnteros(numero1, numero2);
-    cout << Resultado << endl;
-
-    float num1 = ingresarFloat("ingrese un valor flotante: ");
-    cout << endl;
-    float num2 = ingresarFloat("Ingrese un valor flotante: ");
-    cout << endl;
-    float Respuesta = SumarFlotantes(num1, num2);
-    cout << endl;
-    cout << Respuesta << endl;
-
-
-
-    /*cout << imprimirNumeros << endl;
-    char imprimirLetras = ingresarLetra("ingrese letras: ");
-    cout << endl;
-    cout << imprimirLetras << endl;
-    float imprimirFloats = ingresarFloat("ingrese un valor flotante: ");
-    cout << endl;
-    cout << imprimirFloats << endl;
-    string imprimirString = ingresarString("ingrese un string: ");
-    cout << endl;
-    cout << imprimirString << endl;
-    double imprimirDouble = ingresarDouble("Ingrese un valor double: ");
-    cout << endl << imprimirDouble << endl;*/
-
+    else if (tipo == "double")
+    {
+        double doble;
+        for (int j = 0; cad[j] != '\0'; j++)
+        {
+            doble = atof(cad);
+        }
+        return valor = doble;
+    }
+    else if (tipo == "char")
+    {
+        return valor;
+    }
+    else if (tipo == "string")
+    {
+        for (int j = 0; cad[j] != '\0'; j++)
+        {
+            valor = valor + cad[j];
+        }
+        return valor;
+    }
 }
