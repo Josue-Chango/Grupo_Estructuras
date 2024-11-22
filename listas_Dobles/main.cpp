ac8623568247b14f1d87e9bfe0bcf95136a37081
Josue-Chango <josuechango21@hotmail.com>
2024-11-22 08:20:01 -0500

    vale listas dobles

--- listas_Dobles/main.cpp
+++ listas_Dobles/main.cpp
@@ -3,12 +3,14 @@
  * ALUMNOS:  Leonardo Obando, William Leon
  * FECHA CREACION: Miercoles, 21 de noviembre de 2022
  * FECHA MODIFICACION: Miercoles, 23 de noviembre de 2022
- * Enunciado del problema: Crear los métodos Insertar, Buscar
+ * Enunciado del problema: Crear los mï¿½todos Insertar, Buscar
  y Eliminar elementos de una lista Simple, Doble y Circular
  * Nivel: TERCERO     NRC: 7999
  *************************/
-#include "ListasDoble.h"
+#include "ListasDoble.cpp"
+#include "Nodo.cpp"
 #include <stdlib.h>
+#include <iostream>
 using namespace std;
 
 bool esEntero(string);
