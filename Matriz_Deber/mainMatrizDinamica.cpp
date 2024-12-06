de3eb5a9dc7d0e1ccced0495cc460dddb7f220c9
Josue-Chango <josuechango21@hotmail.com>
2024-12-06 14:33:45 -0500

    matriz dinamica

--- Matriz_Deber/mainMatrizDinamica.cpp
+++ Matriz_Deber/mainMatrizDinamica.cpp
@@ -14,15 +14,15 @@ int main(int argc, char** argv) {
     int** mat2 = new int*[3];
 
     for (int i = 0; i < 3; i++) {
-        mat1[i] = new int[3];
-        mat2[i] = new int[3];
+        *(mat1+i) = new int[3];
+        *(mat2+i) = new int[3];
     }
 
     srand(time(NULL));
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            mat1[i][j] = (int)((rand() / 32768.1) * 1000);
-            mat2[i][j] = (int)((rand() / 32768.1) * 1000);
+            *(*(mat1+i)+j) = (int)((rand() / 32768.1) * 1000);
+            *(*(mat2+i)+j) = (int)((rand() / 32768.1) * 1000);
         }
     }
 
@@ -47,6 +47,7 @@ int main(int argc, char** argv) {
         delete[] *(mat1+i);
         delete[] *(mat2+i);
     }
+    system("pause");
     delete[] mat1;
     delete[] mat2;
 
